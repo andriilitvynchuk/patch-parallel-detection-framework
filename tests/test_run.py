6ea3500d1f202f0d2cb0f14921a6bbb24179d570
Andrii Litvynchuk <andrii.litvynchuk@leiainc.com>
2022-04-13 19:40:37 +0300

    fix run

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,6 +3,8 @@ import tempfile
 from pathlib import Path
 from typing import Generator, List
 
+import cv2
+import numpy as np
 import pytest
 
 
@@ -18,8 +20,21 @@ def logs_folder(custom_tmp_path: Path) -> Path:
 
 
 @pytest.fixture(scope="session")
-def overrides(logs_folder: Path) -> List[str]:
+def video(custom_tmp_path: Path) -> Path:
+    video_path = custom_tmp_path / "video.mkv"
+    width, height = 1280, 720
+    video_writer = cv2.VideoWriter(str(video_path), cv2.VideoWriter_fourcc(*"XVID"), 30, (1280, 720), isColor=True)
+    number_of_frames = 150
+    for index in range(number_of_frames):
+        sample = np.random.randint(0, 255, size=(height, width, 3), dtype=np.uint8)
+        video_writer.write(sample)
+    return video_path
+
+
+@pytest.fixture(scope="session")
+def overrides(logs_folder: Path, video: Path) -> List[str]:
     overrides = [f"hydra.run.dir={logs_folder}"]
+    overrides.append(f"cameras.video.path={video}")
     return overrides
 
 
