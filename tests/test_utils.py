6e6ad39c6abef81ba89d0d5c0653225df889825a
Andrii Litvynchuk <andrii.litvynchuk@leiainc.com>
2022-04-14 12:25:34 +0300

    add exception tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -14,3 +14,9 @@ def test_crop_n_parts(batch: torch.Tensor, n_crops: int) -> None:
     result = crop_n_parts(batch, n_crops=n_crops)
     b, c, h, w = batch.shape
     assert result.shape == (b, n_crops, c, h // (int(n_crops**0.5)), w // (int(n_crops**0.5)))
+
+
+@pytest.mark.parametrize("n_crops", [2, 9])
+def test_crop_n_parts_erros(batch: torch.Tensor, n_crops: int) -> None:
+    with pytest.raises(ValueError):
+        crop_n_parts(batch, n_crops=n_crops)
