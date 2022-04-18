f7f2d00ed9a7d2747e9ace07964157b102c9c973
Andrii Litvynchuk <andrii.litvynchuk@leiainc.com>
2022-04-18 15:41:22 +0300

    small improvements

--- dronedet/utils/config.py
+++ dronedet/utils/config.py
@@ -11,30 +11,40 @@ def patch_empty_params(cfg: DictConfig) -> DictConfig:
     Find all key-value pair where value is None and input empty dict instead of None
     """
     for key, value in cfg.items():
-        cfg[key] = dict() if "params" in key and value is None else value  # type: ignore
         value_type = type(value)
         if value_type is DictConfig:
             cfg[key] = patch_empty_params(value)
         elif value_type is ListConfig:
             cfg[key] = [patch_empty_params(element) if type(element) is DictConfig else element for element in value]
+        else:
+            cfg[key] = dict() if "params" in key and value is None else value  # type: ignore
     return cfg
 
 
+def patch_single_path(key: Any, value: Any, original_cwd: str) -> str:
+    if "path" in key and key != "dirpath" and value is not None and not os.path.isabs(value):  # type: ignore
+        value = os.path.join(original_cwd, value)
+    return value
+
+
 def patch_relative_paths(cfg: DictConfig, original_cwd: str) -> DictConfig:
+    """
+    Change all relative paths -> absolute paths to make it work with hydra.
+    key must contain "path" string
+    """
     for key, value in cfg.items():
-        if "path" in key and key != "dirpath" and value is not None:  # type: ignore
-            if type(value) == ListConfig:
-                cfg[key] = [os.path.join(original_cwd, item) if not os.path.isabs(item) else item for item in value]
-            elif not os.path.isabs(value):
-                cfg[key] = os.path.join(original_cwd, value)
         value_type = type(value)
         if value_type is DictConfig:
             cfg[key] = patch_relative_paths(value, original_cwd)
         elif value_type is ListConfig:
             cfg[key] = [
-                patch_relative_paths(element, original_cwd) if type(element) is DictConfig else element
+                patch_relative_paths(element, original_cwd)
+                if type(element) is DictConfig
+                else patch_single_path(key, element, original_cwd)
                 for element in value
             ]
+        else:
+            cfg[key] = patch_single_path(key, value, original_cwd)
     return cfg
 
 
