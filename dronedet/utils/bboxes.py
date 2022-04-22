4a1df798f756062ed8c89fca7ff42631681cde89
Andrii <andrii.litvynchuk@leiainc.com>
2022-04-22 15:39:53 +0000

    update bboxes

--- dronedet/utils/bboxes.py
+++ dronedet/utils/bboxes.py
@@ -44,15 +44,14 @@ def scale_bboxes_torch(
 ) -> torch.Tensor:
     h_scale = output_size[0] / input_size[0]
     w_scale = output_size[1] / input_size[1]
-    scale_tensor = torch.tensor([w_scale, h_scale, w_scale, h_scale]).view(1, -1)
-    bboxes[:, :4] *= scale_tensor.to(bboxes.device).to(bboxes.dtype)
+    bboxes[:, :4] *= torch.tensor([[w_scale, h_scale, w_scale, h_scale]]).to(bboxes.device).to(bboxes.dtype)
     return bboxes
 
 
 def scale_bboxes_numpy(bboxes: np.ndarray, input_size: Tuple[int, ...], output_size: Tuple[int, ...]) -> np.ndarray:
     h_scale = output_size[0] / input_size[0]
     w_scale = output_size[1] / input_size[1]
-    bboxes[:, :4] *= np.array([w_scale, h_scale, w_scale, h_scale]).reshape(1, -1).astype(bboxes.dtype)
+    bboxes[:, :4] *= np.array([[w_scale, h_scale, w_scale, h_scale]]).astype(bboxes.dtype)
     return bboxes
 
 
